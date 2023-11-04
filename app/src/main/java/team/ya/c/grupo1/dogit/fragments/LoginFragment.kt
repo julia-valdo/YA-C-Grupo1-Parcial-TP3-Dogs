74629878ca62e072dbf06e14f0e17ca419c8036d
Nelo <devneloper@hotmail.com>
2023-11-04 19:38:39 -0300

    Creación de fragmentos para Login, Register, RegisterData, Navegación del Login y configuraciones sobre activity_login

---
+++ app/src/main/java/team/ya/c/grupo1/dogit/fragments/LoginFragment.kt
@@ -0,0 +1,20 @@
+package team.ya.c.grupo1.dogit.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import team.ya.c.grupo1.dogit.R
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+}
