965dbb9307cf95dc713575b8d17906a4dd58476d
Julia <valdojuli@hotmail.com>
2023-11-05 18:08:11 -0300

    Agrego email del dueño al perro

--- app/src/main/java/team/ya/c/grupo1/dogit/activities/LoginActivity.kt
+++ app/src/main/java/team/ya/c/grupo1/dogit/activities/LoginActivity.kt
@@ -2,16 +2,11 @@ package team.ya.c.grupo1.dogit.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
 import team.ya.c.grupo1.dogit.R
-import team.ya.c.grupo1.dogit.entities.ThemeProviderEntity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        val theme = ThemeProviderEntity(this).getThemeFromPreferences()
-        AppCompatDelegate.setDefaultNightMode(theme)
     }
 }
--- app/src/main/java/team/ya/c/grupo1/dogit/fragments/PublicationFragment.kt
+++ app/src/main/java/team/ya/c/grupo1/dogit/fragments/PublicationFragment.kt
@@ -14,7 +14,6 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.findNavController
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import team.ya.c.grupo1.dogit.R
 import team.ya.c.grupo1.dogit.activities.MainActivity
@@ -141,9 +140,8 @@ class PublicationFragment : Fragment() {
         val description = binding.editTxtPublicationDogDescription.text.toString()
         val id = UUID.randomUUID().toString()
         val adopterEmail = ""
-        val ownerEmail = FirebaseAuth.getInstance().currentUser?.email?: ""
 
-        val dog = DogEntity(name, race, subrace, age, gender, description, weight, location, images, adopterName, id, mutableListOf<String>(), adopterEmail, ownerEmail)
+        val dog = DogEntity(name, race, subrace, age, gender, description, weight, location, images, adopterName, id, mutableListOf<String>(), adopterEmail)
 
         return if (validateFields(dog)) dog else null
     }
